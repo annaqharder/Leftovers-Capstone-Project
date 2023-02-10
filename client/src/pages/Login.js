24289e43f1dc234ae139af7f1a3bb81c0df2ae9a
Anna Harder <annaqharder@gmail.com>
2023-02-10 14:49:43 -0700

    start tailwind signin page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -63,60 +63,84 @@ function Login() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h2>welcome back!</h2>
-
-                    <div>
-                        <div className="wrapper">
-                            <label>Email: </label>
-                            <div className="input-field">
-                                <input
-                                    type="text"
-                                    inputMode="email"
-                                    name="email"
-                                    // placeholder="enter your email..."
-                                    value={credentials.email}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
+        <section>
+            <div class="container px-6 py-12 h-full">
+                <div class = "flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                    <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+                        <img src="./images/restaurant13.webp" alt="loginImg"/>
                     </div>
-
                     <div>
+                        <div>
+                            <h2>Please Sign In</h2>
+                        </div>
+                        <div>
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <br></br>
+                                    <div className="wrapper">
+                                        <div className="input-field">
+                                            <input
+                                                type="text"
+                                                inputMode="email"
+                                                name="email"
+                                                placeholder="Email"
+                                                value={credentials.email}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <br></br>
+                                    <div className="wrapper">
+                                        <div className='input-field'>
+                                            <input
+                                                type={type}
+                                                name="password"
+                                                placeholder="Password"
+                                                value={credentials.password}
+                                                onChange={handleChange}
+                                                autoComplete="current-password"
+                                            />
+                                            <span onClick={handleToggle}><Icon icon={icon} size={25}/></span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div>
+                                        <input
+                                            type="checkbox"
+                                        />
+                                        <label>Remember me</label>
+                                    </div>
+                                    <a href="#!"> Forgot Password?</a>
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    class="inline-block px-7 py-3 bg-zinc text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-red hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                                >LOG IN</button>
+                            </form>
+                        </div>
+
                         <br></br>
-                        <div className="wrapper">
-                        <label>Password: </label>
-                            <div className='input-field'>
-                                <input
-                                    type={type}
-                                    name="password"
-                                    placeholder="enter your password..."
-                                    value={credentials.password}
-                                    onChange={handleChange}
-                                    autoComplete="current-password"
-                                />
-                                <span onClick={handleToggle}><Icon icon={icon} size={25}/></span>
-                            </div>
+                        <div>
+                            <label>Don't have an account?</label>
+                            <Link onClick={handleToSignupPage}>Sign up</Link>
                         </div>
+                        </div>
+                        </div>
+
+                        <div>
+                            {errors.map((err) => (
+                                <p key={err}>{err}</p>
+                            ))}
+                        </div>
+
                     </div>
-
-                    <br></br>
-                    <button type="submit">Login</button>
-            </form>
-            <br></br>
-            <div>
-                <label>Don't have an account?</label>
-                <Link onClick={handleToSignupPage}>Sign up</Link>
-            </div>
-
-            <div>
-                {errors.map((err) => (
-                    <p key={err}>{err}</p>
-                ))}
-            </div>
-
-        </div>
+        </section>
     );
 }
 
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      'zinc': '#171717',
+      'red': '#B23822',
+      'white': '#fafafa'
+    },
+
+    extend: {},
+  },
+
+  plugins: [],
+}
